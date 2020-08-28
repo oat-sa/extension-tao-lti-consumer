20e2cdc528017b884e1b29118cada285ce690ae7
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-08-28 13:14:06 +0200

    Refactor - Lti1p3ReplaceResultParser

--- model/result/operations/replace/Service/Lti1p3ReplaceResultParser.php
+++ model/result/operations/replace/Service/Lti1p3ReplaceResultParser.php
@@ -22,9 +22,9 @@ declare(strict_types=1);
 
 namespace oat\taoLtiConsumer\model\result\operations\replace\Service;
 
+use OAT\Library\Lti1p3Core\Service\Server\Validator\AccessTokenRequestValidator;
 use oat\oatbox\service\ConfigurableService;
 use oat\taoLti\models\classes\LtiProvider\LtiProviderService;
-use oat\taoLti\models\classes\Security\DataAccess\Service\AccessTokenRequestValidator;
 use oat\taoLtiConsumer\model\result\messages\LisOutcomeRequestParser;
 use oat\taoLtiConsumer\model\result\operations\replace\ReplaceResultOperationRequest;
 use oat\taoLtiConsumer\model\result\ParsingException;
@@ -42,10 +42,10 @@ class Lti1p3ReplaceResultParser extends ConfigurableService implements ReplaceRe
         $result = $this->getAccessTokenRequestValidator()->validate($request);
 
         if ($result->hasError() || $result->getRegistration() === null) {
-            throw new tao_models_classes_UserException('message to change'); #todo: change message
+            throw new tao_models_classes_UserException(sprintf('Access Token Validation failed. %s', $result->getError()));
         }
 
-        $parsedPayload = $this->getRequestParser()->parse((string)$request->getBody()); #todo rename
+        $parsedPayload = $this->getLisOutcomeRequestParser()->parse((string)$request->getBody());
 
         //todo: implement correct LtiProvider retrieve
         $ltiProvider = $this->getLtiProviderService()->findAll();
@@ -54,7 +54,7 @@ class Lti1p3ReplaceResultParser extends ConfigurableService implements ReplaceRe
         return new ReplaceResultOperationRequest($parsedPayload, $ltiProvider);
     }
 
-    private function getRequestParser(): LisOutcomeRequestParser
+    private function getLisOutcomeRequestParser(): LisOutcomeRequestParser
     {
         return $this->getServiceLocator()->get(LisOutcomeRequestParser::class);
     }
